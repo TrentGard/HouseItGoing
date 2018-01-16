c4855e3b191ab2ee13bd86d55a66deb26bd20b22
thesides <aaronm20392@gmail.com>
2018-01-16 17:56:21 -0600

    table models created; mysql db created

---
+++ house-it-going/server.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var db= require("./models");
+
+var PORT = process.env.PORT || 3000;
+var app = express();
+
+db.sequelize.sync({force: true}).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
