cd7157b89fd406c9354f7981de620b1395649a18
Jordan Thaler <jordanpthaler@gmail.com>
2018-01-17 20:16:09 -0600

    Add Home.js for test, add Okta code

--- house-it-going/src/App.js
+++ house-it-going/src/App.js
@@ -1,19 +1,43 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Dashboard from "../pages/Dashboard";
-import UserProfile from "../pages/UserProfile";
-import Nav from "./components/Nav";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Security, ImplicitCallback } from '@okta/okta-react';
+import Home from './Home';
 
-const App = () =>
-  <Router>
-    <div>
-      <Nav />
-      <Switch>
-        <Route exact path="/" component={Dashboard} />
-        <Route exact path="/dash" component={Dashboard} />
-        <Route exact path="/dash/:id" component={UserProfile} />
-      </Switch>
-    </div>
-  </Router>;
+const config = {
+  issuer: 'https://dev-478651.oktapreview.com/oauth2/default',
+  redirect_uri: window.location.origin + '/implicit/callback',
+  client_id: '0oadmccgkao8Etxn10h7'
+}
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Security issuer={config.issuer}
+                  client_id={config.client_id}
+                  redirect_uri={config.redirect_uri}
+        >
+          <Route path='/' exact={true} component={Home}/>
+          <Route path='/implicit/callback' component={ImplicitCallback}/>
+        </Security>
+      </Router>
+    );
+  }
+}
 
 export default App;
