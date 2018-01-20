1390a5a6d94ca5535a54fa40c87f48afc94d94b9
thesides <aaronm20392@gmail.com>
2018-01-19 19:36:05 -0600

    API working with hardcode - see Dashboard.js and componentDidMount function to change hardcode arguments

--- house-it-going/src/pages/Dashboard/Dashboard.js
+++ house-it-going/src/pages/Dashboard/Dashboard.js
@@ -3,8 +3,35 @@ import Jumbotron from "../../components/Jumbotron";
 import Cards from '../../components/Cards';
 import Table from "../../components/Table";
 import { Col, Row, Container } from "../../components/Grid";
+import API from "../../utils/API";
+
 
 class Dashboard extends Component {
+  state = {
+        result: {},
+        zipCode: "",
+        mfi: ""
+    };
+
+    componentDidMount() {
+        this.searchListings("78722")
+    }
+
+    searchListings(zipCode) {
+        API.search(zipCode)
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
+
+    }
+
+    // Access data points on returned JSON from COA API 
+//res.data[x].unit_type
+//res.data[x].affordability_end_year
+//res.data[x].location_address
+//res.data[x].location_zip
+//res.data[x].housing_type
+//res.data[x].total_affordable_units
+
   render() {
     return (
       <div>
---
+++ house-it-going/src/utils/API.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+const BASEURL = "https://data.austintexas.gov/resource/2drp-3fg9.json?status=Project Complete&$where=affordability_end_year>2019"
+const APIKEY = "&$$app_token=EArK5werWrK5LH6M7XAjxbY31"
+const mfiPart1 = "&$where=units_"
+const mfiPart2 = "_mfi > 0"
+const zip = "&zip_code="
+
+export default {
+	search: function (zipCode) {
+		return axios.get(BASEURL + zip + zipCode + APIKEY);
+	}
+};
+
+
+
+// Access data points on returned JSON from COA API
+//res.data[1].unit_type
+//res.data[1].affordability_end_year
+//res.data[1].location_address
+//res.data[1].location_zip
+//res.data[1].housing_type
+//res.data[1].total_affordable_units
