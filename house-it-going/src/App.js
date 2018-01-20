2f700f3eb07b338f72adeb24b5f982cec02e9052
Jordan Thaler <jordanpthaler@gmail.com>
2018-01-20 10:44:46 -0600

    Cleaned up App.js and removed duplicate class

--- house-it-going/src/App.js
+++ house-it-going/src/App.js
@@ -44,36 +44,4 @@ class App extends Component {
   }
 }
 
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <h1 className="App-title">Welcome to React</h1>
-//         </header>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Security issuer={config.issuer}
-                  client_id={config.client_id}
-                  redirect_uri={config.redirect_uri}
-        >
-          <Route path='/' exact={true} component={Home}/>
-          <Route path='/implicit/callback' component={ImplicitCallback}/>
-        </Security>
-      </Router>
-    );
-  }
-}
-
 export default App;
