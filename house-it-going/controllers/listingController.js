37079714fc7e63e0552f14df80a4a0c16408691c
thesides <aaronm20392@gmail.com>
2018-01-21 16:43:49 -0600

    save listing db manipulation code written but not tested due to server connection issue

--- house-it-going/controllers/listingController.js
+++ house-it-going/controllers/listingController.js
@@ -2,16 +2,20 @@ const db = require("../models");
 
 module.exports = {
 	create: function(req, res) {
-		console.log("BLAHAHAHAHAHAHA" + req.body)
+
+		console.log("<<<<--->>>>" + req.body)
+
 		db.SavedListing.create({
 			propertyId: req.body.property_id,
 			address: req.body.address,
 			zip: req.body.zip_code,
 			councilDistrict: req.body,council_district
 			// UserId: req.params.id
-	}).then(function(data) {
-		res.send(data);
-		console.log(data);
-	})
+		}).then(function(data) {
+			res.json(data);
+			console.log(data);
+		}).catch(function (error) {
+			console.log(error)
+		})
 	}
 };
