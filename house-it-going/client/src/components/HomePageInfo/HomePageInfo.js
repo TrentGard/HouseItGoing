418952867a6a17618e307e7fae565f75fc8d9529
thesides <aaronm20392@gmail.com>
2018-01-27 10:38:54 -0600

    working app

--- house-it-going/client/src/components/HomePageInfo/HomePageInfo.js
+++ house-it-going/client/src/components/HomePageInfo/HomePageInfo.js
@@ -3,41 +3,39 @@ import { Link } from "react-router-dom";
 import API from "../../utils/API";
 import {
   Button,
+  Container,
   Grid,
   Header,
   Segment
 } from 'semantic-ui-react';
-		
+        
 
 class HomePageInfo extends Component {
 
-	render() {
-		return (
-			<div>	
-			    <Segment 
-				    style={{ minHeight: 400, padding: '8em 0em' }}
-				    vertical
-			    >
-			      <Grid container stackable verticalAlign='middle'>
-			        <Grid.Row>
-			          <Grid.Column width={14}>
-			            <Header as='h3' style={{ fontSize: '2em' }}>Need Affordable Housing?</Header>
-			            <p style={{ fontSize: '1.33em' }}>
-			              Search for properties based on the required data
-			            </p>
-			          </Grid.Column>
-			        </Grid.Row>
-			        
-			        <Grid.Row>
-			          <Grid.Column textAlign='center'>
-			            <Button size='huge' as={Link} to='/dashboard'>Get Started!</Button>
-			          </Grid.Column>
-			        </Grid.Row>
-			      </Grid>
-			    </Segment>
-			</div>          
-		);
-	};
+    render() {
+        return (
+            <div>    
+                <Segment 
+                    style={{ minHeight: 400, padding: '8em 0em' }}
+                    vertical
+                >
+                    <Container text>
+                        <Header 
+                            as='h2' 
+                            content="Need Affordable Housing?" 
+                            style={{ fontSize: '2em', textAlign: 'center' }} />
+                        <Header
+                            as='h3'
+                            content='Search for properties based on the required data'
+                            style={{ fontSize: '1.7em', fontWeight: 'normal', textAlign: 'center' }}
+                          />    
+                        <Button style={{ marginLeft: 250,textAlign: 'center' }} size='huge' as={Link} to='/dashboard'>Get Started!</Button>
+                        
+                    </Container>
+                </Segment>
+            </div>          
+        );
+    };
 };
 
 export default HomePageInfo;
